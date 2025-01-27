6b65ca5a7985fac66093f0b9bf1c79e26255663c
Sandeepgujjari <35698768+Sandeepgujjari@users.noreply.github.com>
2025-01-27 12:41:15 -0600

    depndency updated

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "storageaccount" {
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   container_names          = var.container_names
-  depends_on = [ module.Resource_Group ]
+  depends_on               = [module.Resource_Group]
 }
 module "KeyVault" {
   source               = "./Modules/Azure_KeyVault"
@@ -21,7 +21,7 @@ module "KeyVault" {
   keyvault_soft_delete = var.keyvault_soft_delete
   keyvault_sku         = var.keyvault_sku
   object_ids           = var.object_ids
-  depends_on = [ module.Resource_Group ]
+  depends_on           = [module.Resource_Group]
 }
 module "vnet" {
   source              = "./Modules/Azure_Vnet"
@@ -32,18 +32,18 @@ module "vnet" {
 
   subnets = {
     "web-subnet" = {
-      name            = "web-subnet"
-      address_prefix  = "10.10.1.0/24"
-      delegation      = null
+      name           = "web-subnet"
+      address_prefix = "10.10.1.0/24"
+      delegation     = null
     },
     "db-subnet" = {
-      name            = "db-subnet"
-      address_prefix  = "10.10.2.0/24"
+      name           = "db-subnet"
+      address_prefix = "10.10.2.0/24"
       delegation = [
         {
           name = "db-delegation"
           service_delegation = {
-            name    = "Microsoft.DBforMySQL/flexibleServers"
+            name = "Microsoft.DBforMySQL/flexibleServers"
             actions = [
               "Microsoft.Network/virtualNetworks/subnets/action",
               "Microsoft.Network/virtualNetworks/subnets/join/action"
@@ -70,6 +70,7 @@ module "application_gateway" {
   frontend_port       = var.frontend_port
   backend_port        = var.backend_port
   tags                = var.tags
+  depends_on          = [module.Resource_Group]
 }
 
 module "azure_lb" {
@@ -89,33 +90,33 @@ module "azure_lb" {
   lb_rule_protocol      = var.lb_rule_protocol
   lb_rule_frontend_port = var.lb_rule_frontend_port
   lb_rule_backend_port  = var.lb_rule_backend_port
-  depends_on = [ module.Resource_Group ]
+  depends_on            = [module.Resource_Group]
 }
- module "web_vm" {
-   source                     = "./Modules/Azure_Linux_VM"
-   count                      = 1
-   depends_on                 = [module.vnet, module.log_analytics,module.KeyVault]
-   vm_name                    = "${var.linux_web_vm_name}0${count.index + 1}"
-   vm_rg_location             = var.location
-   vm_rg_Name                 = var.resource_group_name
-   vm_Size                    = var.vm_Size
-   vm_subnet_name             = "web-subnet"
-   vnet_name                  = var.vnet_name
-   vnet_rg_name               = var.resource_group_name
-   image_reference            = var.linux_image_reference
-   keyvault_name              = var.keyvault_name
-   keyvault_rg_name           = var.resource_group_name
-   data_disks                 = var.linux_data_disks
-   log_analytics_workspace_id = module.log_analytics.workspace_id
-   log_analytics_primary_key  = module.log_analytics.workspace_shared_key
- }
+module "web_vm" {
+  source                     = "./Modules/Azure_Linux_VM"
+  count                      = 1
+  depends_on                 = [module.vnet, module.log_analytics, module.KeyVault]
+  vm_name                    = "${var.linux_web_vm_name}0${count.index + 1}"
+  vm_rg_location             = var.location
+  vm_rg_Name                 = var.resource_group_name
+  vm_Size                    = var.vm_Size
+  vm_subnet_name             = "web-subnet"
+  vnet_name                  = var.vnet_name
+  vnet_rg_name               = var.resource_group_name
+  image_reference            = var.linux_image_reference
+  keyvault_name              = var.keyvault_name
+  keyvault_rg_name           = var.resource_group_name
+  data_disks                 = var.linux_data_disks
+  log_analytics_workspace_id = module.log_analytics.workspace_id
+  log_analytics_primary_key  = module.log_analytics.workspace_shared_key
+}
 
 module "log_analytics" {
   source                    = "./Modules/Azure_log_Analytics"
   log_analytics_Rg_name     = var.resource_group_name
   log_WorkspaceName         = var.log_WorkspaceName
   log_analytics_Rg_location = var.log_analytics_Rg_location
-  depends_on = [ module.Resource_Group ]
+  depends_on                = [module.Resource_Group]
 }
 
 # module "mysql" {
@@ -148,6 +149,6 @@ module "mysql_flexible" {
   availability_zone          = var.mysql_flexible_availability_zone
   mysql_flexible_databases   = var.mysql_flexible_databases
   sku_name                   = var.mysql_flexible_sku_name
-  standby_availability_zone = var.mysql_flexible_standby_availability_zone
-  depends_on = [ module.Resource_Group, module.vnet ]      
+  standby_availability_zone  = var.mysql_flexible_standby_availability_zone
+  depends_on                 = [module.Resource_Group, module.vnet]
 }
